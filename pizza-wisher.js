cfcb33a7721e735c50776520b85529d3f7e3afd7
michaelrichey <michael@tofugu.com>
2019-12-30 16:46:35 -0500

    add docs and code

---
+++ pizza-wisher.js
@@ -0,0 +1,9 @@
+const pizzaList = [
+  ''
+]
+
+document.getElementById('pepperoni').onclick = function () {
+  pizzaList.push()
+  console.log(`Your pizza contains:`)
+  console.log(`${pizzaList}`)
+}
