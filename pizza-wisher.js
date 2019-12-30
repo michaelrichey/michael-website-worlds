17d04af2ee5dc92f19acd3659466f11cb500cb6c
michaelrichey <michael@tofugu.com>
2019-12-30 16:56:21 -0500

    change button output

--- pizza-wisher.js
+++ pizza-wisher.js
@@ -2,8 +2,11 @@ const pizzaList = [
   ''
 ]
 
-document.getElementById('pepperoni').onclick = function () {
-  pizzaList.push()
-  console.log(`Your pizza contains:`)
-  console.log(`${pizzaList}`)
-}
+let output = document.getElementById("output");
+output.innerHTML = "hello world";
+
+//document.getElementById('pepperoni').onclick = function () {
+//  pizzaList.push()
+//  console.log(`Your pizza contains:`)
+//  console.log(`${pizzaList}`)
+//}
