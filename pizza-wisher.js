5c86af732d616e5912428e6e81512b0c3486a960
michaelrichey <michael@tofugu.com>
2020-01-03 16:50:15 -0500

    add text reveal function

--- pizza-wisher.js
+++ pizza-wisher.js
@@ -1,12 +1,16 @@
-const pizzaList = [
-  ''
-]
+const cheatz = [{
+  name: 'Unlock 2X Bazooka',
+  code: 'A + B + ↓C + L + R + START'
+}, {
+  name: 'Tiny Ammo Crates',
+  code: 'Z + A + ←C + R + R + START'
+}]
 
-let output = document.getElementById("output");
-output.innerHTML = "hello world";
+document.querySelector('#output').addEventListener('click', function (e) {
+  cheatz.forEach(function (cheatObject, index) {
+    const showText = document.createElement('p')
+    showText.textContent = `${cheatObject.name} --> ${cheatObject.code}`
+    document.querySelector('body').appendChild(showText)
+  })
 
-//document.getElementById('pepperoni').onclick = function () {
-//  pizzaList.push()
-//  console.log(`Your pizza contains:`)
-//  console.log(`${pizzaList}`)
-//}
+})
