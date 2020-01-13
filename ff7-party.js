bc4f496a9299beb39cd90eb2b5cb2bcf4626f30c
michaelrichey <michael@tofugu.com>
2020-01-13 16:10:45 -0500

    add files, array

---
+++ ff7-party.js
@@ -0,0 +1,56 @@
+const party = [
+  {
+    name: "Cloud Strife",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Barrett Wallace",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Tifa Lockhart",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Aeris Gainsborough",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Red XIII",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Cait Sith",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Cid Highwind",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Yuffie Kisagari",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+  {
+    name: "Vincent Valentine",
+    weapon: asdf,
+    class: asdf,
+    hometown: asdf
+  },
+]
