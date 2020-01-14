7c79555601097bc609458f140049d642c8ff1b1f
michaelrichey <michael@tofugu.com>
2020-01-14 17:35:03 -0500

    add topping submission

---
+++ pizza-order.js
@@ -0,0 +1,31 @@
+const ingredients = []
+
+
+const message = function (x) {
+  x.forEach(function (item) {
+    const generateText = document.createElement('p')
+    generateText.innerText = `You have added: ${item.name}`
+    document.querySelector("#welcome").appendChild(generateText)
+  })
+}
+
+message(ingredients)
+
+document.querySelector("#form").addEventListener('submit', function (e) {
+  e.preventDefault()
+  ingredients.push(
+    {
+      name: e.target.elements.toppings.value,
+      price: 'asdf'
+    }
+  )
+  console.log(ingredients)
+  e.target.elements.toppings.value = ''
+  message(ingredients)
+})
+
+
+
+// const generateText = document.createElement('p')
+// generateText.textContent = `you have todos left to do`
+// document.querySelector('#welcome').appendChild(generateText)
