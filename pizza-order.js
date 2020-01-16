5140cdfebd953f11894d26885b10da526fcfa921
michaelrichey <michael@tofugu.com>
2020-01-16 17:35:56 -0500

    add final box

--- pizza-order.js
+++ pizza-order.js
@@ -1,9 +1,22 @@
-const ingredients = []
+const ingredients = [
+  {
+    name: 'wine',
+    price: 'asdf'
+  },
+  {
+    name: 'brine',
+    price: 'asdf'
+  },
+  {
+    name: 'swine',
+    price: 'asdf'
+  },
+]
 
 
 const message = function (x) {
 
-  document.querySelector('#welcome').innerHTML = ''
+  document.querySelector('#welcome').innerHTML = '<button id="submit-toppings">SUBMIT ORDER</button>'
 
   x.forEach(function (item) {
     const generateText = document.createElement('p')
@@ -20,15 +33,22 @@ document.querySelector("#form").addEventListener('submit', function (e) {
     {
       name: e.target.elements.toppings.value,
       price: 'asdf'
-    }
+    },
   )
   console.log(ingredients)
   e.target.elements.toppings.value = ''
   message(ingredients)
 })
 
+document.querySelector("#submit-toppings").addEventListener("click", function (e) {
+  const finalOrder = document.createElement('p')
+  finalOrder.textContent = 'Your final pizza will have on top of it:'
+  document.querySelector('.box3').appendChild(finalOrder)
 
-
-// const generateText = document.createElement('p')
-// generateText.textContent = `you have todos left to do`
-// document.querySelector('#welcome').appendChild(generateText)
+  ingredients.forEach(function (item) {
+    const toppingsList = document.createElement('p')
+    toppingsList.textContent = item.name
+    document.querySelector(".box3").appendChild(toppingsList)
+  })
+  console.log(ingredients)
+})
